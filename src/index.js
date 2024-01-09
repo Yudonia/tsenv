0010a2aaa3ad9bc08244c9c2e0be15eb82860736
EEprotocol <kei310c@outlook.com>
2024-01-09 10:22:02 +0900

    [change] index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import p5 from "p5";
+import math from "math";
 
-document.body.style.margin="0";
+document.body.style.margin = "0";
 
-export const sketch = (p)=>{
+export const sketch = (p) => {
 
-p.setup=()=> {
-  
-   p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(255);
-
-}
-
-p.draw=()=>{
-  if (p.mouseIsPressed){
-    p.fill(0);
-    // console.log(p.mouseIsPressed())
-  }else{
-    p.fill(255);
-    // console.log(p.mouseIsPressed())
+  function normtr(x, y, r, ud) {
+    p.triangle(x, y, x + r, y, x + r / 2, y + ud * r * 3 ** (0.5) / 2)
   }
-  p.ellipse(p.mouseX,p.mouseY,80,80);
-}
+
+  p.setup = () => {
+
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background(255);
+    x=100;
+    y=100;
+
+  }
+
+  p.draw = () => {
+    if (p.mouseIsPressed) {
+      p.fill(0);
+      // console.log(p.mouseIsPressed())
+    } else {
+      p.fill(255);
+      // console.log(p.mouseIsPressed())
+      console.log(p.mouseY)
+    }
+    p.ellipse(p.mouseX, p.mouseY, 80, 80);
+    // p.triangle(p.mouseX, p.mouseY, p.mouseX+300, p.mouseY, p.mouseX+300/2, p.mouseY+300*1.7/2)
+    normtr(p.mouseX, p.mouseY, 300, -1);
+  }
 }
 
-const app= new p5(sketch,document.body)
+const app = new p5(sketch, document.body)
